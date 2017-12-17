6b7c272a413084a709da5085df3cced5ac680e68
nicknameismos <mynameissarawut@gmail.com>
2017-12-17 12:50:29 +0700

    bite

---
+++ src/app/main/content/sample/i18n/en.ts
@@ -0,0 +1,16 @@
+export const locale = {
+    lang: 'en',
+    data: {
+        'SAMPLE': {
+            'HELLO': 'Hello, World!'
+        },
+        'SHOP': {
+            'GROUP': {
+                'ALLSHOP': 'All',
+                'NEWSHOP': 'New',
+                'OFFICIALSHOP': 'Official',
+                'OUTJOINSHOP': 'Outer Join'
+            }
+        }
+    }
+};
